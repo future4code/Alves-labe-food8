ea6f92edcfdb658be01ca92db48710d56e8467a4
Ayrton Medrano <mrayrtonmedrano@gmail.com>
2022-08-01 16:39:37 -0300

    Criação das rotas e paginas+ constants

---
+++ labe-food/src/routes/Coordinator.js
@@ -0,0 +1,35 @@
+export const goToFeed = navigate => {
+  navigate('/')
+}
+
+export const goToLogin = navigate => {
+  navigate('/login')
+}
+export const goToSignUp = navigate => {
+  navigate('/signup')
+}
+export const goToAdressForm = navigate => {
+  navigate('/adressForm')
+}
+
+export const goToCart = navigate => {
+  navigate('/cart')
+}
+export const goToEditAdress = navigate => {
+  navigate('/editAdress')
+}
+export const goToEditProfile = navigate => {
+  navigate('/editProfile')
+}
+export const goToProfile = navigate => {
+  navigate('/profile')
+}
+export const goToRestaurant = (navigate, id) => {
+  navigate(`/restaurant/${id}`)
+}
+export const goToSplash = navigate => {
+  navigate('/futureEatsC')
+}
+export const goBack = navigate => {
+  navigate(-1)
+}
