5dc8c340b63bdaff74f5bcd9094613b2349aec82
Ayrton Medrano <mrayrtonmedrano@gmail.com>
2022-08-02 15:47:30 -0300

    Criação do GlobalState e renderizacao dos produtos

---
+++ labe-food/src/global/GlobalState.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import GlobalStateContext from './GlobalStateContext'
+import { useState } from 'react'
+
+const GlobalState = props => {
+  const [restaurantDetails, setRestaurantsDetails] = useState([])
+  const states = { restaurantDetails }
+  const setters = { setRestaurantsDetails }
+
+  return (
+    <GlobalStateContext.Provider value={{ states, setters }}>
+      {props.children}
+    </GlobalStateContext.Provider>
+  )
+}
+export default GlobalState
